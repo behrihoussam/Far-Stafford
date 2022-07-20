24e44fcfd78823ee6b6940c9320d410bfadc4d17
behrihoussam <45970611+behrihoussam@users.noreply.github.com>
2022-07-20 15:08:19 +0100

    Add files via upload

---
+++ Scripts/scrollreveal.js
@@ -0,0 +1,31 @@
+
+ScrollReveal({
+    reset: true,
+    distance: "200px",
+    delay: 100,
+    duration: 1000
+});
+
+ScrollReveal().reveal(".main_header--slogan", { origin: "left" });
+ScrollReveal().reveal(".main_header--blob", { origin: "right" });
+ScrollReveal().reveal(".main_header--image", { origin: "bottom" });
+
+ScrollReveal().reveal(".about_us--title", { origin: "top" });
+ScrollReveal().reveal(".about_us--blob", { origin: "left" });
+ScrollReveal().reveal(".about_us--image", { origin: "left" });
+ScrollReveal().reveal(".about_us--description", { origin: "right" });
+
+ScrollReveal().reveal(".about_us--bottom-section_rectangle", { origin: "left" });
+ScrollReveal().reveal(".about_us--bottom-section_image", { origin: "right" });
+
+ScrollReveal().reveal(".our_values--title", { origin: "left" });
+ScrollReveal().reveal(".our_values--blob", { origin: "right" });
+
+ScrollReveal().reveal(".our_services--title", { origin: "top" });
+ScrollReveal().reveal(".our_services--lower-section_image", { origin: "left" });
+ScrollReveal().reveal(".our_services--lower-section_description", { origin: "right" });
+
+ScrollReveal().reveal(".our_solutions", { origin: "top" });
+
+ScrollReveal().reveal(".our_team--title", { origin: "top" });
+ScrollReveal().reveal(".our_leader", { origin: "bottom" });
